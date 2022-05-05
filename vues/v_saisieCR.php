0911f86c857a04ea7452d6b4b974c9cfdcb21786
Sokhna <sokhnamay66@gmail.com>
2022-05-05 16:57:37 +0200

    saisieCR

--- vues/v_saisieCR.php
+++ vues/v_saisieCR.php
@@ -1,63 +1,66 @@
-<form action="index.php?do=lesEntreprises&action=validerAjouterEntreprise" method="post">
+<form action="index.php?uc=CompteRendu&action=validerSaisieCR" method="post">
 <div id="contenu">
 
-<!-- Création d'un formulaire permettant l'ajout d'une entreprise dans la base de données -->
+<!-- Création d'un formulaire permettant l'ajout d'un CR dans la base de données -->
 
-      <h2>Ajouter une entreprise</h2>
+      <h2>Rapport de visite</h2>
 
       <table class = "ajouter">
 
 
-      <!-- Création d'une zone de texte "Raison sociale" -->
-      <tr> <td class = "ajouter"> Raison sociale : * </td>
-      <td class = "ajouter"> <input size="22" type="text" name="raisonsociale" required> </td> </tr>
+      <!-- Création d'une zone de texte "numero" -->
+      <tr> <td class = "ajouter"> Numero : * </td>
+      <td class = "ajouter"> <input size="22" type="text" name="numero" required> </td> </tr>
 
 
-      <!-- Création d'une zone de texte "Adresse" -->
-      <tr> <td class = "ajouter"> Adresse : * </td>
-      <td class = "ajouter"><input size="22" type="text" name="adresse" required>  </td> </tr>
+      <!-- Création d'une zone de texte "Date visite" -->
+      <tr> <td class = "ajouter"> Date Visite : * </td>
+      <td class = "ajouter"><input size="22" type="text" name="dateVisite" required>  </td> </tr>
 
 
-      <!-- Création d'une zone de texte "Code postal" -->
-      <tr> <td class = "ajouter"> Code postal : * </td>
-      <td class = "ajouter"><input size="22" type="text" name="codepostal" pattern="[0-9]{5}" required>  </td></tr>
+      <!-- Création d'une liste déroulante avec une zone de texte possédant l'id du praticien en "value", le nom et le prénom
+du praticien donnent des détails sur le praticien-->
+<label for ="choix_praticien"> Praticien : </label>
+      <input list="praticiens" type="text" name="choix_praticien" required autocomplete = 'off'>
+      <datalist id="praticiens">
+      <?php  foreach ($lesEPraticiens as $unPraticien) {
+                         $idPraticien = $unPraticien['pra_num'];
+                         $idPraticien = $unPraticien['pra_nom'];
+                         $idPraticien = $unPraticien['pra_prenom'];
+        
+      echo'<option value='.$idPraticien.'>'.$nomPraticien.' '.$prenomPraticien.'</option>';
+}
+?>
+</datalist></select> </td>
 
 
-      <!-- Création d'une zone de texte "Ville" -->
-      <tr> <td class = "ajouter"> Ville : * </td>
+      <!-- Création d'une zone de texte "coefficient" -->
+      <tr> <td class = "ajouter"> coefficient: * </td>
       <td class = "ajouter"><input size="22" type="text" name="ville" required>  </td> </tr>
 
 
-      <!-- Création d'une zone de texte "Téléphone" -->
-      <tr> <td class = "ajouter"> Téléphone : * </td>
-      <td class = "ajouter"><input size="22" type="text" name="telephone" pattern="^(0)[1-9]([-. ]?[0-9]{2}){4}$" required>  </td> 
+      <!-- Création d'une case à cocher "remplacant" -->
+      <tr> <td class = "ajouter"> Remplaçant : * </td>
+      <td class = "ajouter"><input size="22" type="checkbox" name="remplacant" required>  </td> 
       </tr>
 
 
-      <!-- Création d'une zone de texte "Fax" non obligatoire -->
-      <tr> <td class = "ajouter"> Fax : </td>
-      <td class = "ajouter"><input size="22" type="text" name="fax" pattern="^(0)[1-9]([-. ]?[0-9]{2}){4}$">  </td> </tr>
+      <!-- Création d'une zone de texte "date" non obligatoire -->
+      <tr> <td class = "ajouter"> Date de visite : * </td>
+      <td class = "ajouter"><input size = "22" type="date" name="dateVisite" required> </td> </tr>
 
 
-      <!-- Création d'une zone de texte "Mail" -->
-      <tr> <td class = "ajouter"> Mail : * </td>
-      <td class = "ajouter"><input size="27" type="email" name = "mail" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,}" required> </td> </tr>
+      <!-- Création d'une liste déroulante "motif" (a voir apres)-->
+     
 
 
-      <!-- Création d'une liste déroulante avec une zone de texte possédant l'id du statut d'entreprise en "value", le libellé 
-      donne des détails sur le statut -->
-      <label for ="choix_statuts"> <tr> <td class = "ajouter"> Statut : * </td><td class = "ajouter"></label>
-      <input list="statuts" size="22" type="text" name="choix_statut" required autocomplete = 'off'>
-      <datalist id="statuts">
-      <?php  foreach ($lesStatuts as $unStatut) {
-            $id = $unStatut['idstatut'];
-            $libelle = $unStatut['libellestatut'];
+      <!-- Création d'une zone de texte "bilan" -->
+      <tr> <td class = "ajouter"> Bilan: * </td>
+      <td class = "ajouter"><input size="22" type="text" name="bilan" required>  </td> </tr>
 
-      echo'<option value='.$id.'>'.$libelle.'</option>';
-}
 
 ?>
-      </datalist></select> </td>
+      
       <td>
       <input type="button" onclick="window.location.replace('http://localhost/application/GestionDesStagesV0/index.php?do=lesEntreprises&action=statut')" value="Ajouter un statut d'entreprise" /> </td>
       </tr></table> 
