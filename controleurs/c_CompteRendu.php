249fb60960aaaf5e0be9722a082edc3675e509ed
Sokhna <sokhnamay66@gmail.com>
2022-05-06 16:56:20 +0200

    modifCR

--- controleurs/c_CompteRendu.php
+++ controleurs/c_CompteRendu.php
@@ -1,29 +1,33 @@
 ﻿<?php
 include("vues/v_sommaire.php");
+
+
 $action = $_REQUEST['action'];
 switch($action){ 
     // Envoie sur la vue saisirCR permettant la saisie d'un CR
-    case 'ajouterCR':{
-	    include("vues/v_sommaire.php");
+    case 'saisirCR':{
+	    // include("vues/v_sommaire.php");
         $lesPraticiens=$pdo->getLesPraticiens();
         $lesProduits=$pdo->getLesProduits();
-        include("vues/v_saisirCR.php");
+        include("vues/v_saisieCR.php");
 	    break;
 	}
         
 	case 'validerSaisieCR':{
-		include("vues/v_sommaire.php");   
+		// include("vues/v_sommaire.php");   
         $Matricule = $_POST['matricule'];
+        $Numero= $_POST['numVisite'];
         $dateVisite = $_POST['dateVisite'];
         $praticien = $_POST['choix_praticien'];
         $coef = $_POST['coef'];
         $remplacant = $_POST['remplacant'];
+        $Motif=$_POST['motif'];
         $Echantillon= $_POST['nbreEchantillon'];
         $Produit= $_POST['documentation'];
         $bilan= $_POST['choix_produit'];
         $documentation= $_POST['documentation'];
 
-        $leCR=$pdo->AjouterCR($Numero, $dateVisite, $praticien, $coef, $remplacant , $Echantillon, $Produit, $bilan, $documentation);
+        $leCR=$pdo->AjouterCR($Matricule, $Numero, $dateVisite, $praticien, $coef, $remplacant , $Motif, $Echantillon, $Produit, $bilan, $documentation);
         $message = "Compte rendu ajouté avec succès";
        // include("vues/v_Message.php"); 
         break;
--- vues/v_saisieCR.php
+++ vues/v_saisieCR.php
@@ -1,27 +1,33 @@
+<main>
+     <h2>Rapport de visite</h2>
+      <aside>
 <form action="index.php?uc=CompteRendu&action=validerSaisieCR" method="post">
-<div id="contenu">
+
 
 <!-- Création d'un formulaire permettant l'ajout d'un CR dans la base de données -->
+  
 
-      <h2>Rapport de visite</h2>
-
-      <table class = "ajouter">
+      <table>
 
 
       <!-- Création d'une zone de texte "matricule" -->
-      <tr> <td class = "ajouter"> Matricule: * </td>
-      <td class = "ajouter"> <input size="22" type="text" name="matricule" required> </td> </tr>
+      <tr> <td> Matricule: * </td>
+      <td> <input size="22" type="text" name="matricule" required> </td> </tr>
+
+      <!-- Création d'une zone de texte "numero de visite" -->
+      <tr> <td> NUmero: * </td>
+      <td> <input size="22" type="text" name="numVisite" required> </td> </tr>
 
 
       <!-- Création d'une zone de texte "Date visite" -->
-      <tr> <td class = "ajouter"> Date Visite : * </td>
-      <td class = "ajouter"><input size="22" type="date" name="dateVisite" required>  </td> </tr>
+      <tr> <td > Date Visite : * </td>
+      <td ><input size="22" type="date" name="dateVisite" required>  </td> </tr>
 
 
       <!-- Création d'une liste déroulante avec une zone de texte possédant l'id du praticien en "value", le nom et le prénom
 du praticien donnent des détails sur le praticien-->
-<tr> <td class = "ajouter"> <label for ="choix_praticien"> Praticien : *</td></label>
-<td class = "ajouter"><input list="praticiens" type="text" name="choix_praticien" required autocomplete = 'off'>
+<tr> <td> <label for ="choix_praticien"> Praticien : *</td></label>
+<td ><input list="praticiens" type="text" name="choix_praticien" required autocomplete = 'off'>
       <datalist id="praticiens">
       <?php  foreach ($lesPraticiens as $unPraticien) {
                          $idPraticien = $unPraticien['PRA_NUM'];
@@ -35,24 +41,24 @@ du praticien donnent des détails sur le praticien-->
 
 
       <!-- Création d'une zone de texte "coefficient" -->
-      <tr> <td class = "ajouter"> coefficient: * </td>
-      <td class = "ajouter"><input size="22" type="text" name="coef" required>  </td> </tr>
+      <tr> <td > coefficient: * </td>
+      <td><input size="22" type="text" name="coef" required>  </td> </tr>
 
 
       <!-- Création d'une case à cocher "remplacant" -->
-      <tr> <td class = "ajouter"> Remplaçant : * </td>
-      <td class = "ajouter"><input size="22" type="checkbox" name="remplacant" required>  </td> 
+      <tr> <td > Remplaçant : * </td>
+      <td><input size="22" type="checkbox" name="remplacant" required>  </td> 
       </tr>
 
 
       <!-- Création d'une liste déroulante "motif" -->
-      <tr> <td class = "ajouter"> Motif : * </td>
-      <td class = "ajouter"><input size="22" type="text" name="motif" required>  </td> </tr>
+      <tr> <td > Motif : * </td>
+      <td ><input size="22" type="text" name="motif" required>  </td> </tr>
 
 
       <!-- Création d'une liste déroulante avec une zone de texte possédant l'id du médicament en "value", le nom du médicament -->
-<tr> <td class = "ajouter"> <label for ="choix_produit"> Produits présentés : *</td></label>
-<td class = "ajouter"><input list="produits" multiple="oui" type="text" name="choix_produit" autocomplete = 'off'>
+<tr> <td> <label for ="choix_produit"> Produits présentés : *</td></label>
+<td><input list="produits" multiple="oui" type="text" name="choix_produit" autocomplete = 'off'>
       <datalist id="produits">
       <?php  foreach ($lesProduits as $unProduit) {
                          $idMedicament = $unMedicament['MED_DEPOTLEGAL'];
@@ -64,38 +70,29 @@ du praticien donnent des détails sur le praticien-->
 </datalist> </td> </tr>
 
     <!-- Création d'un champ "nombre d'échantillons" -->
-    <tr> <td class = "ajouter"> Nombre d'échantillons : * </td>
-    <td class = "ajouter"><input type="number" name="nbreEchantillon" title="Nombre" value="5" min="0" max="20" step="1" required=""> </td>
+    <tr> <td> Nombre d'échantillons : * </td>
+    <td><input type="number" name="nbreEchantillon" title="Nombre" value="5" min="0" max="20" step="1" required=""> </td>
     </tr>
 
 
       <!-- Création d'une zone de texte "bilan" -->
-      <tr> <td class = "ajouter"> Bilan: * </td>
-      <td class = "ajouter"><input size="22" type="text" name="bilan" required>  </td> </tr>
+      
+      <tr> <td> <label for="bilan">Bilan : </label> </td>
 
-      <tr> <td class = "ajouter"> <label for="bilan">Bilan : </label> </td>
-
-        <td class = "ajouter"> <textarea  name="bilan" rows="4" cols="40"></textarea> </td>
+        <td> <textarea  name="bilan" rows="4" cols="40"></textarea> </td>
       </tr>
 
       <!-- Création d'une case à cocher "documentation" -->
-      <tr> <td class = "ajouter"> Documentation offerte : * </td>
-      <td class = "ajouter"><input size="22" type="checkbox" name="documentation" required>  </td> 
+      <tr> <td> Documentation offerte : * </td>
+      <td><input size="22" type="checkbox" name="documentation" required>  </td> 
       </tr>
-
-
-
-
-
-      
-      <!-- <td>
-      <input type="button" onclick="window.location.replace('http://localhost/application/GestionDesStagesV0/index.php?do=lesEntreprises&action=statut')" value="Ajouter un statut d'entreprise" /> </td> -->
       </table>
 
 <!-- Aide pour l'utilisateur signifiant que les champs précédés d'une astérisque sont obligatoires -->
       * : Obligatoire
       <p>
-      <input type="submit" name="Valider">  
-      <input type="reset" name="Annuler">  
-      </div>
-</form>
+      <input type="submit" name="Valider" value="Valider">  
+      <input type="reset" name="Annuler" value="Annuler">  
+      </form>
+      </aside>
+    </main>
