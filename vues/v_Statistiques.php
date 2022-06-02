2891dc5b9fb37fb7e03d9308575fd7a6a5f4b39f
sokhna maye fall <sokhnamay66@gmail.com>
2022-06-02 17:36:25 +0200

    exemple stats

--- vues/v_Statistiques.php
+++ vues/v_Statistiques.php
@@ -1,3 +1,53 @@
+<!--  premier exemple -->
+
+<?php
+ 
+$dataPoints = array(
+	array("label"=> "Education", "y"=> 284935, "indexLabel"=> "Highest"),
+	array("label"=> "Entertainment", "y"=> 256548),
+	array("label"=> "Lifestyle", "y"=> 245214),
+	array("label"=> "Business", "y"=> 233464),
+	array("label"=> "Music & Audio", "y"=> 200285),
+	array("label"=> "Personalization", "y"=> 194422),
+	array("label"=> "Tools", "y"=> 180337),
+	array("label"=> "Books & Reference", "y"=> 172340),
+	array("label"=> "Travel & Local", "y"=> 118187),
+	array("label"=> "Puzzle", "y"=> 107530, "indexLabel"=> "Lowest")
+);
+	
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title: {
+		text: "Top 10 Google Play Categories - till 2017"
+	},
+	axisY: {
+		title: "Number of Apps"
+	},
+	data: [{
+		type: "column",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="position: relative; height:40vh; width:80vw"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>                              
+
+<!--  deuxième exemple -->
 <div id="contenu">
     <h1>Statistiques de <?php echo $_SESSION['STAT_TYPELIEU'] . strtoupper($_SESSION['localite']); ?></h1>
     <br>
@@ -7,3 +57,5 @@
 <div class="pie animate no-round" style="--p:80;--c:orange;"> 80%</div>
 <div class="pie animate" style="--p:90;--c:lightgreen"> 90%</div>
 </div>
+                             
+
