9a499fea128a7b30c40814c8425a94073751e364
sabri.ryckebusch <sabri.ryckebusch@gmail.com>
2023-02-23 16:42:59 +0100

    UpdateZones

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -13,8 +13,9 @@ public class Jeu {
     
     private void creerCarte() {
         Zone [] zones = new Zone [4];
-        zones[0] = new Zone("le couloir", "Couloir.jpg" );
-        zones[1] = new Zone("l'escalier", "Escalier.jpg" );
+
+        /* zones[0] = new Zone("le couloir", "Couloir.jpg" );
+        / zones[1] = new Zone("l'escalier", "Escalier.jpg" );
         zones[2] = new Zone("la grande salle", "GrandeSalle.jpg" );
         zones[3] = new Zone("la salle à manger", "SalleAManger.jpg" );
         zones[0].ajouteSortie(Sortie.EST, zones[1]);
@@ -23,7 +24,9 @@ public class Jeu {
         zones[2].ajouteSortie(Sortie.OUEST, zones[1]);
         zones[3].ajouteSortie(Sortie.NORD, zones[1]);
         zones[1].ajouteSortie(Sortie.SUD, zones[3]);
-        zoneCourante = zones[1]; 
+        zoneCourante = zones[1];   */
+
+        zones[1] = new Zone("l'ile", "/exterieur/ile.png");
     }
 
     private void afficherLocalisation() {
