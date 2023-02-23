04365802f4aadc119b04ec9095f683b8953564fe
sabrirkb <sabri.ryckebusch@gmail.com>
2023-02-23 18:49:46 +0100

    Ajout de commentaires

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -5,12 +5,18 @@ public class Jeu {
 	private Zone zoneCourante;
     
     public Jeu() {
-        // Inclure un menu de départ pour permettre au joueur de reprendre une sauvegarde / créer une nouvelle partie
-        
-        // N'utiliser cette fonction QUE lorsque le joueur décide de créer une nouvelle partie.
-        creerCarte();
 
-        gui = null;
+        // --------------------------------
+        // Inclure un menu de départ -> reprendre une sauvegarde / créer une nouvelle partie
+        // La fonction init(); affichera l'interface du menu principal;
+        // --------------------------------
+        // Si le joueur décide de créer une nouvelle partie, on fera appel à la
+        // fonction newGame() qui executera le code suivant :
+        // 
+            creerCarte(); // -> Inclure dans la fonction newGame()
+            gui = null; // -> Inclure dans la fonction newGame()
+        //
+        // --------------------------------
     }
 
     public void setGUI( GUI g) { gui = g; afficherMessageDeBienvenue(); }
@@ -18,25 +24,34 @@ public class Jeu {
     private void creerCarte() {
         Zone [] zones = new Zone [4];
 
-        /* zones[0] = new Zone("le couloir", "Couloir.jpg" );
-        / zones[1] = new Zone("l'escalier", "Escalier.jpg" );
-        zones[2] = new Zone("la grande salle", "GrandeSalle.jpg" );
-        zones[3] = new Zone("la salle à manger", "SalleAManger.jpg" );
-        zones[0].ajouteSortie(Sortie.EST, zones[1]);
-        zones[1].ajouteSortie(Sortie.OUEST, zones[0]);
-        zones[1].ajouteSortie(Sortie.EST, zones[2]);
-        zones[2].ajouteSortie(Sortie.OUEST, zones[1]);
-        zones[3].ajouteSortie(Sortie.NORD, zones[1]);
-        zones[1].ajouteSortie(Sortie.SUD, zones[3]);
-        zoneCourante = zones[1];   */
+        // EXEMPLES DE CREATION D'UNE NOUVELLE ZONE
+        // zone[x] = new Zone("description de la zone", "src/image.png");
+        // zones[0] = new Zone("le couloir", "Couloir.jpg" );
+        // zones[1] = new Zone("l'escalier", "Escalier.jpg" );
+        // zones[2] = new Zone("la grande salle", "GrandeSalle.jpg" );
+        // zones[3] = new Zone("la salle à manger", "SalleAManger.jpg" );
 
-        // TEST //
-        // zone[0] = new Zone(description: "menu principal", image: "");
+
+        // EXEMPLES D'AJOUT D'UNE SORTIE DANS UNE ZONE EXISTANTE
+        // zone[x].ajouteSortie(Sortie.DIRECTION, zone[y]);
+        // zones[0].ajouteSortie(Sortie.EST, zones[1]);
+        // zones[1].ajouteSortie(Sortie.OUEST, zones[0]);
+        // zones[1].ajouteSortie(Sortie.EST, zones[2]);
+        // zones[2].ajouteSortie(Sortie.OUEST, zones[1]);
+        // zones[3].ajouteSortie(Sortie.NORD, zones[1]);
+        // zones[1].ajouteSortie(Sortie.SUD, zones[3]);
+
+
+        // EXEMPLE DE CHANGEMENT DE LA ZONE COURANTE (QUI S'AFFICHE SUR LE JEU)
+        // zoneCourante = zones[1];   
+
+        // zone[0] = new Zone(description: "menu principal", image: "/interface/menuPrincipal.png");
         zones[1] = new Zone("l'ile de jour", "/exterieur/ile/ileJournee.png");
         zones[2] = new Zone("l'ile de nuit", "/exterieur/ile/ileNuit.png");
 
-        // Mettre la zone de depart sur zones[0] une fois l'interface du menu principal créée
-        zoneCourante = zones[1];
+        // Mettre la zone de depart sur zones[0] lorsque
+        // l'interface du menu principal sera créée
+        zoneCourante = zones[1]; // -> Pour l'instant, on se contentera de mettre zoneCourante sur l'exterieur de la prison
     }
 
     private void afficherLocalisation() {
